9b46ab80d51733e61981c593713b6b04e1267705
prajwal4518 <prajwalpadal17@gmail.com>
2020-02-19 02:19:15 +0530

    Online Portal

---
+++ index.php
@@ -0,0 +1,112 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Home Page</title>
+<link rel="stylesheet" href="header.css">
+<link rel="stylesheet" href="main.css">
+
+<!--The following script tag downloads a font from the Adobe Edge Web Fonts server for use within the web page. We recommend that you do not modify it.-->
+<script>var __adobewebfontsappname__="dreamweaver"</script>
+<script src="http://use.edgefonts.net/aclonica:n4:default.js" type="text/javascript"></script>
+
+</head>
+
+<body>
+	<div class="full-page">
+		<header class="top-header">
+		<div class="heading">
+			<h1>Farm<br>
+			Assistant</h1>
+		</div>
+		  <nav class="top-nav">
+			  <ul>
+					<li><a href="index.html">Home</a></li>
+				    <li><a href="program.hml">Inputs</a></li>
+					<li><a href="schemes.html">Schemes</a></li>
+					<li><a href="About.html">Departments</a></li>
+					<li><a href="contact.html">Projects</a></li>
+					<li><a href="program.hml">Universities</a></li>
+				    <li style="border: none;"><a href="program.hml">Login</a></li>
+				</ul>
+			</nav>
+		</header>
+	<main>	
+		<div class="main-tile">
+			<div class="weather-container">
+				<a class="weatherwidget-io" href="https://forecast7.com/en/19d8875d34/aurangabad/" data-label_1="AURANGABAD" data-label_2="WEATHER" data-icons="Climacons Animated" data-days="5" data-theme="weather_one" >AURANGABAD WEATHER</a>
+				<script>
+				!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src='https://weatherwidget.io/js/widget.min.js';fjs.parentNode.insertBefore(js,fjs);}}(document,'script','weatherwidget-io-js');
+				</script>
+			</div>
+        </div>
+		<p class="text">
+		We have the most listings and constant updates.
+		<br>
+		So you’ll never miss out.
+		</p>
+		<div class="text-line"></div>
+		<div class="main-tile-2">
+			<div class="tile1">
+				<div class="tile1-text">
+					<p>Pradhan Mantri Pik Bima Yojna 2019</p>
+					<a href="https://pmfby.gov.in/ceo/dashboard"><button class="btn">Explore</button></a> <a href="https://pmfby.gov.in/selfRegistration"><button class="btn">Register</button></a>
+				</div>
+				<div class="tile1-img"><img src="images/Crop.jpg" style="background-position: center ;background-size: cover" alt="">
+				</div>
+			</div>
+		
+			<div class="tile2">
+				<div class="tile2-a">
+					<div class="tile2-text">
+					<p>Pradhan Mantri Kisan Bima<br>Yojna 2019</p>
+					<button class="btn">Explore</button> 
+					</div>
+					<div class="tile2-a-img"><img src="images/mofpi.jpg" alt=""></div>
+				</div>
+				<div class="tile2-b">
+					<p class="tile2-text">Mega Food Park</p>
+					<button class="btn">Explore</button> 
+				</div> 
+			</div>
+	  </div>
+		
+		<p class="text">
+		We have the most listings and constant updates.
+		<br>
+		So you’ll never miss out.
+		</p>
+		<div class="text-line"></div>
+		
+	<div class="tile3">
+		<div class="card one">
+		  <img src="images/culti.jpg" class="tile3-img" alt="" style="width:100%">
+		  <h1>Cultivation Practices</h1>
+		  <p class="title">A Practice to Improve Yield</p>
+		  
+		  <a href="cultivation.html"><p><button>Explore</button></p></a>
+		</div>
+		
+		
+		<div class="card two">
+		  <img src="images/wt.png" class="tile3-img" alt="" style="width:100%">
+		  <h1>Weather Forecast</h1>
+		  <p class="title">What Weather say...</p>
+		  
+		  <a href="cultivation.html"><p><button>Explore</button></p></a>
+		</div>
+		
+		<div class="card three" style="margin-right: 0px;">
+		  <img src="images/vege.jpg" class="tile3-img" alt="" style="width:100%">
+		  <h1>Buy And Sell</h1>
+		  <p class="title">Direct to HOME By Farmers</p>
+		  
+		  <a href="cultivation.html"><p><button>Contact</button></p></a>
+		</div>
+		
+	</div>	
+		
+	</main>
+	</div>
+</body>
+</html>
